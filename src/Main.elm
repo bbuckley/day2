e86950ac7e1a13461032ca2fd2fb119c871af66b
brian buckley <briankbuckley@gmail.com>
2019-09-03 19:39:07 -0400

    works now.

--- src/Main.elm
+++ src/Main.elm
@@ -1,21 +1,32 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, h1, img, text)
+import Html exposing (Html, button, div, h1, img, text)
 import Html.Attributes exposing (src)
+import Html.Events exposing (onClick)
 
 
+getAt : Int -> List a -> Maybe a
+getAt idx xs =
+    if idx < 0 then
+        Nothing
 
----- MODEL ----
+    else
+        List.head <| List.drop idx xs
+
+
+curVal : Model -> Maybe Int
+curVal model =
+    getAt (modBy (List.length model.cycle) model.curIndex) model.cycle
 
 
 type alias Model =
-    {}
+    { curIndex : Int, cycle : List Int }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { curIndex = 0, cycle = [ 4, 1, 2 ] }, Cmd.none )
 
 
 
@@ -24,26 +35,37 @@ init =
 
 type Msg
     = NoOp
+    | Cycle
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        Cycle ->
+            ( { model | curIndex = modBy (List.length model.cycle) (model.curIndex + 1) }, Cmd.none )
 
 
 
 ---- VIEW ----
 
 
+h1s : Int -> List (Html Msg)
+h1s n =
+    List.map (\_ -> h1 [] [ text "Your Elm App is working!" ]) (List.range 1 n)
+
+
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
-        , h1 [] [ text "Your Elm App is working!" ]
-        , h1 [] [ text "Your Elm App is working!" ]
-        , h1 [] [ text "Your Elm App is working!" ]
-        ]
+        ([ img [ src "/logo.svg" ] []
+         , h1 [] [ text "Your Elm App is working!" ]
+         , button [ onClick Cycle ] [ "cycle" |> text ]
+         ]
+            ++ h1s (Maybe.withDefault 0 (getAt model.curIndex model.cycle))
+        )
 
 
 
